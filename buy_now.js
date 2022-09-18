719be1836387cf7becb4649e7840bd1d70473300
adaolisanobledinnaya <adanoblee@gmail.com>
2022-09-18 09:21:34 +0100

    first commit

--- buy_now.js
+++ buy_now.js
@@ -1,21 +1,21 @@
-// const button = document.querySelector ('.buy-submit')
+const button = document.querySelector ('.buy-submit')
 
-// const buyNow = () => {
-//    if (userInput.value ==='') {
-//     alert ('Please Input letters')  
-//    }
+const buyNow = () => {
+   if (userInput.value ==='') {
+    alert ('Please Input letters')  
+   }
 
-//    else if (userInput.value ==='YOUR NAME') {
-//     alert ('Please Enter Your custom letters')
-//    }
+   else if (userInput.value ==='YOUR NAME') {
+    alert ('Please Enter Your custom letters')
+   }
 
 
-// else if (userInput.value.length > 15) {
-//     alert ('You have Exceeded number of letters')
-// }
+else if (userInput.value.length > 15) {
+    alert ('You have Exceeded number of letters')
+}
 
-// else {
-//     alert ('Good job')
-// }
-// }
-// button.addEventListener ('click', buyNow)
+else {
+    alert ('Good job')
+}
+}
+button.addEventListener ('click', buyNow)
